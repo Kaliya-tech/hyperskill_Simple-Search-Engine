907791e9af2b037fb03635354a944229011dd881
Kaliya Baibosunova <k.baibosunova@gmail.com>
2021-11-26 18:48:11 +0300

    finish project

--- Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/task/src/search/Main.java
@@ -1,24 +1,25 @@
 package search;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
 
-        String pathToFile = args[1];
-
-        boolean repeatLoop = true;
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        String pathToFile = args[1];
 
-        StringBuilder information = saveTheData(pathToFile);
+        ArrayList<String> information = saveTheData(pathToFile);
+        Map <String, List<Integer>> invertedMap = new HashMap<>();
 
+        invertedIndex(information, invertedMap);
+
+        boolean repeatLoop = true;
         while (repeatLoop) {
 
                 System.out.println();
-                System.out.println("=== Menu ===\n" +
+                System.out.println("\n" + "=== Menu ===\n" +
                         "1. Find a person\n" +
                         "2. Print all people\n" +
                         "0. Exit");
@@ -27,75 +28,202 @@ public class Main {
 
                 switch (menu) {
                     case 1:
-                        System.out.println();
-                        System.out.println("Enter a name or email to search all "
-                                + "suitable people.");
-                        findTheData(scanner, information);
+                        pickTheStrategy(scanner, information);
                         break;
                     case 2:
-                        System.out.println();
-                        System.out.println("=== List of people ===");
-                        System.out.println(information);
-                        //
+                        System.out.println("\n" + "=== List of people ===");
+                        information.stream().forEach(System.out::println);
                         break;
                     case 0:
-                        System.out.println("Bye!");
+                        System.out.println("\n" + "Bye!");
                         repeatLoop = false;
                         break;
                     default:
-                        System.out.println("Incorrect option! Try again.");
+                        System.out.println("\n" + "Incorrect option! Try again.");
                         break;
                 }
             }
         }
 
-    public static StringBuilder saveTheData(String pathToFile) {
+    public static ArrayList saveTheData(String pathToFile) {
 
-        StringBuilder information = new StringBuilder();
+        ArrayList<String> information = new ArrayList<>();
         File file = new File(pathToFile);
 
         try (Scanner sc = new Scanner(file)) {
             while (sc.hasNext()) {
-                information.append(sc.nextLine()).append("\n");
+                String data = sc.nextLine();
+                information.add(data);
             }
-
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
         return information;
     }
 
-//    public static Map<Integer, String> addDataToMap() {
-//
-//    }
+    public static void pickTheStrategy(Scanner scanner,ArrayList<String> information) {
 
-    public static void findTheData(Scanner scanner,StringBuilder information) {
+        System.out.println("\n" + "Select a matching strategy: ALL, ANY, NONE");
 
         scanner.nextLine(); // zero element
-        String query = scanner.nextLine();
+        String strategy = scanner.nextLine();
 
-        String dataStr = information.toString();
-        String[] data = dataStr.split("\n");
+        System.out.println("\n" + "Enter a name or email to search all "
+                + "suitable people.");
 
-        List<String> results = new ArrayList<>();
-
-        for (String value: data) {
-            String[] searchQuery = query.split("");
-            if (searchQuery.length == 1) {
-                System.out.println("No matching people found.");
+        switch (strategy) {
+            case "ALL":
+                allStrategy(scanner, information);
                 break;
-            } else if (value.toLowerCase().contains(query.strip().toLowerCase())) {
-                results.add(value);
-            }
+            case "ANY":
+                anyStrategy(scanner, information);
+                break;
+            case "NONE":
+                noneStrategy(scanner, information);
+                break;
+        }
+    }
+
+    public static void allStrategy(Scanner scanner, ArrayList<String> information) {
+
+        String query = scanner.nextLine().trim().toLowerCase(Locale.ROOT);
+        String[] queryArr = query.split("");
+        List<String> output = new ArrayList<>();
+
+        int size = queryArr.length;
+
+        switch (size) {
+            case 1:
+                for (String inf : information) {
+                    if (inf.toLowerCase().contains(queryArr[0])) {
+                        output.add(inf);
+                    }
+                }
+                break;
+            case 2:
+                for (String inf : information) {
+                    if (inf.toLowerCase().contains(queryArr[0])
+                            && inf.toLowerCase().contains(queryArr[1])) {
+                        output.add(inf);
+                    }
+                }
+                break;
+            case 3:
+                for (String inf : information) {
+                    if (inf.toLowerCase().contains(queryArr[0])
+                            && inf.toLowerCase().contains(queryArr[1])
+                            && inf.toLowerCase().contains(queryArr[2])) {
+                        output.add(inf);
+                    }
+                }
         }
 
-        // check List size
-        if (results.size() == 0) {
-            System.out.println("No matching people found.");
+        //CHECK LIST SIZE
+        if (output.size() == 0) {
+            System.out.println("\n" + "No matching people found.");
         } else {
-            System.out.println(results.size() + " persons found:");
-            for (String found: results) {
-                System.out.println(found);
+            System.out.println("\n" + output.size() + " persons found:");
+            output.stream().forEach(System.out::println);
+        }
+    }
+
+    public static void anyStrategy(Scanner scanner, ArrayList<String> information) {
+
+        String query = scanner.nextLine().trim().toLowerCase(Locale.ROOT);
+        String[] queryArr = query.split(" ");
+        List<String> output = new ArrayList<>();
+
+        int size = queryArr.length;
+
+        switch (size) {
+            case 1:
+                for (String inf : information) {
+                    if (inf.toLowerCase().contains(queryArr[0])) {
+                        output.add(inf);
+                    }
+                }
+                break;
+            case 2:
+                for (String inf : information) {
+                    if (inf.toLowerCase().contains(queryArr[0])
+                            || inf.toLowerCase().contains(queryArr[1])) {
+                        output.add(inf);
+                    }
+                }
+                break;
+            case 3:
+                for (String inf : information) {
+                    if (inf.toLowerCase().contains(queryArr[0])
+                            || inf.toLowerCase().contains(queryArr[1])
+                            || inf.toLowerCase().contains(queryArr[2])) {
+                        output.add(inf);
+                    }
+                }
+        }
+
+        //CHECK LIST SIZE
+        if (output.size() == 0) {
+            System.out.println("\n" + "No matching people found.");
+
+        } else {
+            System.out.println("\n" + output.size() + " persons found:");
+            output.stream().forEach(System.out::println);
+        }
+    }
+
+    public static void noneStrategy(Scanner scanner, ArrayList<String> information) {
+
+        String query = scanner.nextLine().trim().toLowerCase(Locale.ROOT);
+        String[] queryArr = query.split(" ");
+        List<String> output = new ArrayList<>();
+
+        int size = queryArr.length;
+
+        switch (size) {
+            case 1:
+                for (String inf : information) {
+                    if (!inf.toLowerCase().contains(queryArr[0])) {
+                        output.add(inf);
+                    }
+                }
+                break;
+            case 2:
+                for (String inf : information) {
+                    if (!inf.toLowerCase().contains(queryArr[0])
+                            && !inf.toLowerCase().contains(queryArr[1])) {
+                        output.add(inf);
+                    }
+                }
+                break;
+            case 3:
+                for (String inf : information) {
+                    if (!inf.toLowerCase().contains(queryArr[0])
+                            && !inf.toLowerCase().contains(queryArr[1])
+                            && !inf.toLowerCase().contains(queryArr[2])) {
+                        output.add(inf);
+                    }
+                }
+        }
+
+        //CHECK LIST SIZE
+        if (output.size() == 0) {
+            System.out.println("\n" + "No matching people found.");
+
+        } else {
+            System.out.println("\n" + output.size() + " persons found:");
+            output.stream().forEach(System.out::println);
+        }
+    }
+
+    public static void invertedIndex(ArrayList<String> information, Map <String, List<Integer>> invertedMap) {
+
+        for (int i = 0; i < information.size(); i++) {
+            String[] words = information.toString().trim().toLowerCase().split(" ");
+            for (String word : words) {
+                if (!invertedMap.containsKey(word)) {
+                    invertedMap.put(word, new ArrayList<>());
+                }
+                invertedMap.get(word).add(i);
             }
         }
     }
